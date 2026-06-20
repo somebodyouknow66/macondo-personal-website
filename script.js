48daaf13fa52ffeb7abcc6a5c37c85fd6d883b9a
Sakhi Akram <sakhiakram.github@outlook.com>
2026-06-20 23:11:58 +0500

    base modal set up

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ document.querySelector('.avatar').addEventListener('click', () => {
     document.querySelector('.burst-menu').classList.toggle('open');
 })
 
+const overlay = document.querySelector('.modal-overlay');
+
+document.querySelectorAll('.dock-item').forEach(btn => {
+    btn.addEventListener('click', () => {
+        const section = btn.dataset.modal;
+        const isOpen = overlay.classList.contains('open');
+        const isSame = overlay.dataset.current === section;
+
+        if (isOpen && isSame) {
+            overlay.classList.remove('open');
+        } else {
+            overlay.dataset.current = section;
+            overlay.classList.add('open');
+        }
+    });
+});
+
+document.querySelector('modal-close').addEventListener('keydown', e => {
+    if (e.key === 'Escape' ) overlay.classList.remove('open');
+});
+
+overlay.addEventListener('click', e => {
+    if (e.target === overlay) overlay.classList.remove('open');
+})
